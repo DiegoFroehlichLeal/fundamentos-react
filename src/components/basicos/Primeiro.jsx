04988a06f9809b8a09e88ddf7414ffe005bc48e2
Diego Froehlich Leal <diegofrleal@gmail.com>
2023-09-06 22:24:50 -0300

    props

---
+++ src/components/basicos/Primeiro.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+export default function Primeiro() {
+    const msg = 'Seja bem vindo!'
+    return (
+        <div>
+            <div>
+                <h2>Primeiro Componente</h2>
+                <p>{msg}</p>
+            </div>
+        </div>
+
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import ReactDom from 'react-dom'
+import ReactDOM from 'react-dom'
+import React from 'react'
+import "./index.css"
+import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
+ReactDOM.render(
+    <div>
+        <Primeiro></Primeiro>
+        <ComParametro titulo="primeiro titulo" subtitulo="segundo titulo"></ComParametro>    
+    </div>,
+    document.getElementById('root')
+)
